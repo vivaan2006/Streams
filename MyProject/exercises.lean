0e188e889e5c9831c539b34c0307f6ceef8c0484
Vivaan <vivaanrajesh2006@gmail.com>
2023-11-29 22:08:47 -0700

    exercises attempt(bad)

--- MyProject/exercises.lean
+++ MyProject/exercises.lean
@@ -120,16 +120,66 @@ theorem bisimulation_proof_two : is_bisimulation R := by
           intro h₁
           apply Exists.intro State.s2
           exact ⟨Transition.s1_to_s2 , rfl⟩
-  case s2 =>
-    sorry
+          -- attempt for 1
+    case s2 =>
+      apply And.intro
+      case left =>
+        intro s₁' h₁
+        cases h₁
+        case s1_to_s2 =>
+          apply Exists.intro State.s2
+          apply And.intro
+          case left => exact Transition.s1_to_s2
+          case right => rfl
+        case s2_to_s1 =>
+          apply Exists.intro State.s1
+          apply And.intro
+          case left => exact Transition.s2_to_s1
+          case right => rfl
+      case right =>
+        intro s₂'
+        cases s₂'
+        case s1 =>
+          intro h₁
+          contradiction
+        case s2 =>
+          intro h₁
+          apply Exists.intro State.s2
+          exact ⟨Transition.s1_to_s2, rfl⟩
     -- Exercise 1: try to complete on your own
 
 
+
+
 -- Execrcise 2 : Complete the more general version
+
+theorem bisimulation_proof_general {StateSpace : Type} {T : Type} (R : StateSpace → StateSpace → Prop) (TransitionFunction : StateSpace → StateSpace → Prop) : Prop :=
+  ∀ (s₁ s₂ : StateSpace),
+    R s₁ s₂ →
+    (∀ (s₁' : StateSpace),
+      TransitionFunction s₁ s₁' →
+      ∃ (s₂' : StateSpace),
+        TransitionFunction s₂ s₂' ∧ R s₁' s₂') ∧
+    ∀ (s₂' : StateSpace),
+      TransitionFunction s₂ s₂' →
+      ∃ (s₁' : StateSpace),
+        TransitionFunction s₁ s₁' ∧ R s₁' s₂'
+
 
 -- Exercise 3: State ->  P(State) --- the one we have right now
 --          Define bisimulation for transition systems of the following type:  State -> ℕ × State
 
 
+def PState := State → Prop
 
-
+def bisimulation_PState (R : PState → PState → Prop) : Prop :=
+  ∀ (s₁ s₂ : State),
+    R (λ x, x = s₁) (λ x, x = s₂) →
+    (∀ (s₁' : State),
+      Transition s₁ s₁' →
+      ∃ (s₂' : State),
+        Transition s₂ s₂' ∧ R (λ x, x = s₁') (λ x, x = s₂')) ∧
+    ∀ (s₂' : State),
+      Transition s₂ s₂' →
+      ∃ (s₁' : State),
+        Transition s₁ s₁' ∧ R (λ x, x = s₁') (λ x, x = s₂')
